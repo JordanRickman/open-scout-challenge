1129749559315f7fdc413d04a7336b14009e94a0
Jordan Rickman <jordan.rickman.42@gmail.com>
2020-11-12 14:07:25 -0500

    Enhancement: Scale video player with card while maintaining aspect ratio.

--- components/OnePagerVideo.tsx
+++ components/OnePagerVideo.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Box, AspectRatioBox } from '@chakra-ui/core';
 // In production, may want to support other video sources (Vimeo, etc.)
 // For this alpha test, all video links in our example data are Youtube,
 //  so import the YouTube-only version, in order to reduce bundle size.
@@ -19,7 +20,13 @@ export const OnePagerVideo = ({
 }: OnePagerVideoProps) => {
   return (
     <ContentCard title='Pitch Video' isLoading={isLoading}>
-      <ReactPlayer url={onePagerData.pitchVideoLink} width="100%" />
+      {/* Using AspectRatioBox, plus 100% height & width on ReactPlayer, means
+           that both height and width of the player will scale if we change
+           the size of the ContentCard (e.g. xl -> lg).
+        TODO Query the YouTube API to get an aspect ratio for the pitch video? */}
+      <AspectRatioBox maxW="100%" ratio={16 / 9}>
+        <Box><ReactPlayer url={onePagerData.pitchVideoLink} width="100%" height="100%" /></Box>
+      </AspectRatioBox>
       {/* TODO Show error message on bad video URL? */}
     </ContentCard>
   );
