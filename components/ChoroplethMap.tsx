0c36c037d471c76003bdd5a4fdc81a6679ff8bce
Jordan Rickman <jordan.rickman.42@gmail.com>
2020-11-13 02:25:25 -0500

    Switch to library react-simple-maps.

--- components/ChoroplethMap.tsx
+++ components/ChoroplethMap.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import Datamap from 'datamaps/dist/datamaps.world.min.js';
 import d3 from 'd3';
+import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
+import worldGeo from '../data/world-110m.json';
 
 import { RegionalUserCount } from '../model/model';
 
@@ -8,85 +9,47 @@ type ChoroplethMapProps = {
   minValue: number;
   maxValue: number;
   regionalUserCounts: RegionalUserCount[];
-}
+};
 
-export class ChoroplethMap extends React.Component {
-  render() {
-    return (
-      <div id="choropleth-map" style={{
-        height: "360px",
-        width: "100%"
-      }}></div>
-    );
-  }
-
-  /* We have to do most of the work in componentDidMount(), because Datamap
-     needs a rendered element to attach to. */
-  componentDidMount() {
-    console.log(this.props); // DEBUG
-    const paletteScale = d3.scale.linear()
-              .domain([this.props.minValue, this.props.maxValue])
+export const ChoroplethMap = ({
+  minValue,
+  maxValue,
+  regionalUserCounts
+}: ChoroplethMapProps) => {
+  const paletteScale = d3.scale.linear()
+              .domain([minValue, maxValue])
               .range(["#FFEFEF", 'DarkRed']); // red palette. TODO style variable.
+  const defaultFill = '#EEE'; // grey
 
-    const dataset: {
-      [countryCode: string]: {
-        value: number;
-        fillColor: string;
-      }
-    } = {};
-    this.props.regionalUserCounts?.forEach((dataPoint) => {
-      dataset[dataPoint.countryCode] = {
-        userCount: dataPoint.userCount,
-        fillColor: paletteScale(dataPoint.userCount)
-      };
-    });
+  const dataset: {
+    [countryCode: string]: {
+      userCount: number;
+      fillColor: string;
+    }
+  } = {};
+  regionalUserCounts?.forEach((dataPoint) => {
+    dataset[dataPoint.countryCode] = {
+      userCount: dataPoint.userCount,
+      fillColor: paletteScale(dataPoint.userCount)
+    };
+  });
 
-    const defaultFill = '#eee'; // grey
-    let map = new Datamap({
-      element: document.getElementById('choropleth-map'),
-      projection: 'mercator',
-      data: dataset,
-      geographyConfig: {
-        borderWidth: 0.5,
-        borderColor: '#444',
-        highlightOnHover: true,
-        highlightBorderWidth: 2,
-        highlightFillColor: function(geo) {
-          return geo.fillColor || defaultFill;
-        },
-        highlightBorderColor: '#000',
-        popupOnHover: true,
-        popupTemplate: function (geo, data) {
-          // don't show tooltip if country isn't present in dataset
-          if (!data) { return; }
-          return (
-            // <ChoroplethTooltip countryName={geo.properties.name} userCount={data.value} />
-            ['<div class="hoverinfo">',
-                    '<strong>', geo.properties.name, '</strong>',
-                    '<br>Count: <strong>', data.userCount, '</strong>',
-                    '</div>'].join('')
-          );
-        }
-      },
-      fills: {
-        defaultFill: defaultFill
-      }
-    });
-  }
-}
-
-type ChoroplethTooltipProps = {
-  countryName: string,
-  userCount: number
-}
-const ChoroplethTooltip = ({
-  countryName,
-  userCount
-}: ChoroplethTooltipProps) => {
   return (
-    <div class="hoverinfo">
-      { countryName }: <br />
-      <strong>{ userCount }</strong> Users
-    </div>
-  );
-}
+    <ComposableMap>
+      <Geographies geography={worldGeo}>
+        {({ geographies }) =>
+          geographies.map((geo) => {
+            const data = dataset[geo.properties.ISO_A3]
+            return (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                fill={data ? data.fillColor : defaultFill}
+              />
+            );
+          })
+        }
+      </Geographies>
+    </ComposableMap>
+  )
+};
