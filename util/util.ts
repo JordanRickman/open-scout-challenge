d9e6d2586b7b0f321307a66049319bac55d8c679
Jordan Rickman <jordan.rickman.42@gmail.com>
2020-11-13 17:19:56 -0500

    Use human-friendly format for smaller (>1 million) user numbers.

--- util/util.ts
+++ util/util.ts
@@ -1,13 +1,17 @@
+/** Small utility functions that are used in multiple places, and don't fit
+  *  anywhere else in the codebase. */
 
-
-export const formatNumberWithCommas = (n: number) => {
-  return String(n); // TODO
+/** Format human-friendly number. Add commas, round to nearest whole number. */
+export const formatNumberBasic = (n: number) => {
+  return Math.round(n).toLocaleString('en');
 };
 
+/** Round a number to the given number of decimal places. */
 export const roundToDecimalPlaces = (n: number, precision: number) => {
   return Math.round(n * (10 ** precision)) / (10 ** precision);
 };
 
+/** Format a human-friendly number, using "Millions" or "Billions" if it's big enough. */
 export const formatUserNumber = (n: number) => {
   if (n > 10 ** 9) {
     return `${roundToDecimalPlaces(n / 10**9, 1)} Billion`;
