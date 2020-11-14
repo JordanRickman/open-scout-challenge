115e317c88e1b18eb951edf63b2fa0275b82892a
Jordan Rickman <jordan.rickman.42@gmail.com>
2020-11-14 07:54:41 -0500

    Bugfix: paid button doesn't work on Vercel due to serverless functions.

--- lib/usePaywall.ts
+++ lib/usePaywall.ts
@@ -8,10 +8,13 @@ export const usePaywall = (onePagerUrl: string): {
   markPaid: () => void;
 } => {
   const [isBlocked, setIsBlocked]: [boolean, any] = useState(false);
-  const [cookies, setCookie] = useCookies(['userId']);
+  const [cookies, setCookie] = useCookies(['userId', 'paid']);
 
   /** Async check if this page is blocked by the paywall. True if blocked. */
   async function checkPaywall(url: string, userId: string = 'new') {
+    if (cookies.paid === 'PAID') {
+      return false;
+    }
     const serverResponse = await fetch(`/api/canRead/${url}/${userId}`);
     const user = await serverResponse.json();
     // Set cookie for new user ID, or refresh cookie to delay expiration.
@@ -25,18 +28,24 @@ export const usePaywall = (onePagerUrl: string): {
 
   useEffect(() => {
     checkPaywall(onePagerUrl, cookies.userId).then((paywallCheck) => setIsBlocked(paywallCheck));
-  }, [onePagerUrl, cookies.userId]);
+  }, [onePagerUrl, cookies.userId, cookies.paid]);
 
-  /** Mark the current user as having paid. */
-  // function markPaid(userId: string) {
-  //   return fetch(`/api/markPaid/${userId}`, {method: 'POST'})
-  //     .then(() => console.log('paid.')); // DEBUG
-  // }
+  /** Mark the current user as having paid.
+   *
+   * To be more secure / harder to bypass, I had been storing
+   * whether the person had paid on the server. However, this doesn't work on
+   * Vercel, because each API route runs in a separate serverless function so
+   * they don't share state unless you integrate a separate datastore.
+   * So at the last minute, switching to storing payment flag in a cookie,
+   * just to get it working.
+   */
   async function markPaid(userId: string) {
-    const serverResponse = await fetch(`/api/markPaid/${userId}`, {method: 'POST'});
-    if (serverResponse.ok) {
-      setIsBlocked(false);
-    }
+    setCookie('paid', 'PAID', {
+      path: '/',
+      maxAge: 60*60*24*365, // Expires in one year
+      sameSite: true
+    });
+    setIsBlocked(false);
   }
 
   return { isBlocked: isBlocked, markPaid: () => markPaid(cookies.userId) };
